cdea61ca98e76e8fe8b2475ee33fab8f8057028c
Julien Capellari <julien@capellari.net>
2020-01-04 23:38:11 +0100

    update: parse gena events

---
+++ network/gena/constants.py
@@ -0,0 +1,4 @@
+# Constants
+XML_GENA_NS = {
+    'event': 'urn:schemas-upnp-org:event-1-0'
+}
--- network/gena/subscription.py
+++ network/gena/subscription.py
@@ -2,16 +2,30 @@ import asyncio
 
 from aiohttp import web, ClientResponse
 from network.base.machine import StateMachine
-from typing import Optional
+from typing import Dict, Optional
+from xml.etree import ElementTree as ET
+
+from .constants import XML_GENA_NS
 
 
 # Class
 class GENASubscription(StateMachine):
+    """
+    class GENASubscription:
+    Represent and manage a GENA subscription.
+
+    Events:
+    - update (values: Dict[str, str], seq: int) : each time a new value is emitted
+    - expired (was: str)                        : each time the subscription goes expired
+    """
+
     def __init__(self, event: str, res: ClientResponse):
         super().__init__('valid')
 
         # Attributes
         self.event = event
+        self._seq = 0
+        self._values = {}  # type: Dict[str, str]
         self.__invalid_handle = None  # type: Optional[asyncio.TimerHandle]
 
         # Parse response
@@ -27,8 +41,32 @@ class GENASubscription(StateMachine):
         return False
 
     # Methods
+    def _parse_xml(self, xml: ET.Element) -> Dict[str, str]:
+        pset = xml.find('event:propertyset', XML_GENA_NS)
+        values = {}
+
+        for prop in pset.findall('event:property', XML_GENA_NS):
+            var = list(prop)[0]
+
+            values[var.tag] = var.text
+
+        return values
+
     def _handler(self, request: web.BaseRequest):
-        pass
+        nt = request.headers.getone('NT')
+        nts = request.headers.getone('NTS')
+
+        if nt == 'upnp:event' and nts == 'upnp:propchange':
+            seq = int(request.headers.getone('SEQ'))
+
+            if seq == 0 or seq > self._seq:
+                data = await request.read()
+                xml = ET.fromstring(data)
+
+                self._seq = seq
+                self._values = self._parse_xml(xml)
+
+                self.emit('update', self._values, seq=seq)
 
     def _update(self, res: ClientResponse):
         # Parse response
@@ -51,9 +89,17 @@ class GENASubscription(StateMachine):
         self.__end()
 
     def __end(self):
-        self.state = 'invalid'
+        self.state = 'expired'
 
     # Properties
     @property
     def expired(self) -> bool:
-        return self.state == 'invalid'
+        return self.state == 'expired'
+
+    @property
+    def seq(self):
+        return self._seq
+
+    @property
+    def value(self):
+        return self._values
